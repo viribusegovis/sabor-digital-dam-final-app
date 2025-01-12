ee977305aed8bbd323f875a0b6c9412544bc58f6
Bruno Freitas <59490875+viribusegovis@users.noreply.github.com>
2025-01-12 18:09:55 +0000

    Created register page and added button to register to Login page

--- app/src/main/java/pt/ipt/dam/sabordigital/LoginActivity.kt
+++ app/src/main/java/pt/ipt/dam/sabordigital/LoginActivity.kt
@@ -1,14 +1,58 @@
 package pt.ipt.dam.sabordigital
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import pt.ipt.dam.sabordigital.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setupLoginButton()
+    }
+
+    private fun setupLoginButton() {
+        binding.loginButton.setOnClickListener {
+            val email = binding.emailInput.text.toString()
+            val password = binding.passwordInput.text.toString()
+
+            if (validarCredenciais(email, password)) {
+                realizarLogin(email, password)
+            }
+        }
+    }
+
+    private fun realizarLogin(email: String, password: String) {
+        lifecycleScope.launch {
+            try {
+                val response = RetrofitInitializer().authService().login(email, password)
+                if (response.isSuccessful) {
+                    val token = response.body()?.accessToken
+                    guardarToken(token)
+                    navegarParaHome()
+                } else {
+                    mostrarErro("Credenciais inválidas")
+                }
+            } catch (e: Exception) {
+                mostrarErro("Erro ao realizar login")
+            }
+        }
+    }
+
+    private fun guardarToken(token: String?) {
+        val sharedPrefs = EncryptedSharedPreferences.create(
+            "auth_prefs",
+            "master_key",
+            this,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+        sharedPrefs.edit().putString("access_token", token).apply()
     }
 }
+
---
+++ app/src/main/java/pt/ipt/dam/sabordigital/RegisterActivity.kt
@@ -0,0 +1,5 @@
+package pt.ipt.dam.sabordigital
+
+class RegisterActivity {
+
+}
